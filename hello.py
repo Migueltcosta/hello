7bec21018aeb8f219d605f2494e7342c60e69387
Migueltcosta <migueltecosta@gmail.com>
2020-05-18 15:46:19 +0100

    First commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+def say_hi():
+	print('Hi, my name is Miguel Costa.')
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Migueltcosta',
+      author_email='Migueltcosta',
+      url='https://github.com/Migueltcosta',
+      py_modules=['hello'],
+     )
